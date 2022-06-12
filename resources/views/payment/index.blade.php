7bb14307224fc592148be73d6bf7aa50512e0a66
Abdul Majid <abdulmajidcse@gmail.com>
2022-06-12 10:09:47 +0600

    app init

---
+++ resources/views/payment/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('headTitle', 'Payment')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Payment List</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
